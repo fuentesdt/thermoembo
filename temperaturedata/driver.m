4e6c518106fa3cf5da608ae6f4612869557e207d
David Fuentes <fuentesdt@gmail.com>
2019-01-10 14:09:29 -0600

    ENH: @sjfahr convert to vtk

--- temperaturedata/driver.m
+++ temperaturedata/driver.m
@@ -10,32 +10,33 @@
 % /FUS4/data2/sfholtz/bitBucket/cressman/MGE_userguide_CJM.m
 % /FUS4/data2/sfholtz/bitBucket/cressman/Mar2018Pub/SaveMGE_script.m
 
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney1_01122017_Exp29/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney1_01242017_Exp31/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney2_01242017_Exp31/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/EXP38_kidney1_03022017/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney1Left_04202017_Exp42/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney2Right_04202017_Exp42/MGE_obj.mat'; % Path to write MGE object to disk
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/EXP38_kidney2_03022017/MGE_obj.mat'; % Path to write MGE object to disk
-
-outputPath = '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney2_01242017_Exp31/MGE_obj_ARMA90_Model1.mat';
 
 
-xxx = load(outputPath)
+outputPathList = {'/FUS4/data2/sfholtz/Cressman/Mar2018Pub/EXP38_kidney1_03022017/MGE_obj_ARMA90_Model1.mat','/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney1Left_04202017_Exp42/MGE_obj_ARMA90_Model1.mat', '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/EXP38_kidney2_03022017/MGE_obj_ARMA90_Model1.mat','/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney2_01242017_Exp31/MGE_obj_ARMA90_Model1.mat', '/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney1_01242017_Exp31/MGE_obj_ARMA90_Model1.mat','/FUS4/data2/sfholtz/Cressman/Mar2018Pub/Kidney2Right_04202017_Exp42/MGE_obj_ARMA90_Model1.mat'}
 
 
-yyy = xxx.img.deltaT;
+for jjj = 1 : length(outputPathList )
+    outputPath = outputPathList{jjj}
+    fullpathsplit =  strsplit(outputPath  ,'/');
+    outputdir = fullpathsplit{ length(fullpathsplit)-1}
+    mkdir(outputdir)
 
+    fulldata = load(outputPath);
+    temperaturedata = fulldata.img.deltaT;
+    
+    
+    header.PixelSpacing   = fulldata.img.hdrEx.PixelSpacing;
+    header.SliceThickness = fulldata.img.sliceThickness ;
+    
+    nsteps = size(temperaturedata ,4);
+    
+    for iii = 1:nsteps
+        fileout = sprintf('%s/temperature.%04d.vtk',outputdir ,iii)
+        saveVTKFile(fileout ,'temperature', temperaturedata(:,:,:,iii), header)
+    end
 
-header.PixelSpacing   = xxx.img.hdrEx.PixelSpacing;
-header.SliceThickness = xxx.img.sliceThickness ;
-
-nsteps = size(yyy,4);
-
-for iii = 1:nsteps
-    fileout = sprintf('temperature.%04d.vtk',iii)
-    saveVTKFile(fileout ,'temperature', yyy(:,:,:,iii), header)
 end
+
 
 
 
