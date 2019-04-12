34d708d9e7e774ffc663dee478a7512dee9574c1
David Fuentes <fuentesdt@gmail.com>
2019-04-12 14:55:08 -0500

    WIP: ..

--- tutorials/phasemesh.py
+++ tutorials/phasemesh.py
@@ -1,4 +1,8 @@
 import os
+import vtk
+import vtk.util.numpy_support as vtkNumPy 
+import numpy as np
+import nibabel as nib  
 
 
 with open("isosurface.1.node") as myfile:
@@ -6,23 +10,36 @@ with open("isosurface.1.node") as myfile:
 
 numnodes = int(lines[0][0] )
 
+## vtkReader = vtk.vtkDataSetReader() 
+## vtkReader.SetFileName( "signed.vtk" ) 
+## vtkReader.Update()
+## imageDataVTK = vtkReader.GetOutput()
+## dimensions = imageDataVTK.GetDimensions()
+## spacing = imageDataVTK.GetSpacing()
+## origin  = imageDataVTK.GetOrigin()
+## print spacing, origin, dimensions
+## #fem.SetImagingDimensions( dimensions ,origin,spacing) 
+## 
+## image_point_data = imageDataVTK.GetPointData() 
+## image_data       = vtkNumPy.vtk_to_numpy( image_point_data.GetArray(0) ) 
+import nibabel as nib  
+imagedata = nib.load("test.nii.gz" )
+numpyimage= imagedata.get_data()
+
 nodes=[]
-for iii in  range(numnodes):
-   nodes.append( (float(lines[iii+1][1]),float(lines[iii+1][2]),float(lines[iii+1][3])) )
+## for iii in  range(numnodes):
+##    nodes.append( (float(lines[iii+1][1]),float(lines[iii+1][2]),float(lines[iii+1][3]), .1) )
 
 #image data
-dim = [110, 165, 5] 
-dim = [20, 20, 5] 
-bb = [[49.217,10.5465,0], [126.558,126.558,25]]
-bb = [ [1.e-3*bb[0][0] , 1.e-3*bb[0][1] , 1.e-3*bb[0][2]], [1.e-3*bb[1][0] , 1.e-3*bb[1][1] , 1.e-3*bb[1][2]] ]
-vox = [0.7031, 0.7031, 5]
+dim = numpyimage.shape
 
 for iii in range(dim[0]):
   for jjj in range(dim[1]):
      for kkk in range(dim[2]):
-         newnode = (bb[0][0] + (bb[1][0]-bb[0][0])/dim[0] * iii,
-                    bb[0][1] + (bb[1][1]-bb[0][1])/dim[1] * jjj,
-                    bb[0][2] + (bb[1][2]-bb[0][2])/dim[2] * kkk)
+         newnode = (numpyimage[iii,jjj,kkk,0,0],
+                    numpyimage[iii,jjj,kkk,0,1],
+                    numpyimage[iii,jjj,kkk,0,2],
+                    numpyimage[iii,jjj,kkk,0,3])
          nodes.append(newnode)
 
 f = open("mytest.node", "w")
@@ -31,4 +48,11 @@ for iii, node in enumerate(nodes):
   f.write("%d %f %f %f\n" % (iii, node[0],node[1],node[2]))
 f.close()
 
+f = open("mytest.mrt", "w")
+f.write("%d 1 \n" % len(nodes))
+for iii, node in enumerate(nodes):
+  f.write("%f\n" % (node[3]))
+f.close()
+
 os.system("tetgen -k mytest.node")
+
