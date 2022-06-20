1a74e41b3b18f2f16fc754436a09b9b532692e04
David Fuentes <fuentesdt@gmail.com>
2022-06-20 17:03:02 -0500

    WIP: loop over cell

---
+++ couplemesh/get1dmesh.py
@@ -0,0 +1,34 @@
+
+import vtk
+# echo vtk version info
+print("using vtk version", vtk.vtkVersion.GetVTKVersion())
+import os
+
+vtkReader = vtk.vtkPolyDataReader()
+vtkReader.SetFileName( "CenterlineComputationModel.vtk" )
+vtkReader.Update()
+DataVTK = vtkReader.GetOutput()
+points =DataVTK.GetPoints()
+npoints =DataVTK.GetNumberOfPoints()
+print(points.GetNumberOfPoints())
+
+inVerts = DataVTK.GetVerts()
+inLines = DataVTK.GetLines()
+inPolys = DataVTK.GetPolys()
+inStrips = DataVTK.GetStrips()
+
+idList = vtk.vtkIdList()
+cellNum = inLines.GetNumberOfCells()
+cellLocation = 0
+for iii in range(cellNum ):
+  inLines.GetCell(cellLocation,idList )
+  numnode = idList.GetNumberOfIds()
+  # https://public.kitware.com/pipermail/vtkusers/2013-January/078002.html
+  cellLocation += 1 + numnode ;
+  print("numnode", numnode )
+  for jjj in range(numnode ):
+    print(idList.GetId(jjj))
+
+
+
+ 
