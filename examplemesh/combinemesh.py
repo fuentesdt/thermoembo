14989ab5d25d065f45f01c3d03ee1b5592a30b70
David Fuentes <fuentesdt@gmail.com>
2022-08-09 15:11:06 -0500

    WIP:

--- examplemesh/combinemesh.py
+++ examplemesh/combinemesh.py
@@ -1,100 +1,95 @@
 # load centerline file and merge with background sphere mesh
-
 import os
 import numpy as np
 import nibabel as nib  
 import vtk
 import ctypes 
 
-
 # c3d temperature.0050.vtk -replace NaN 0 maxtemp.vtk
-
 
 # setup command line parser to control execution
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option( "--file_name",
-                  action="store", dest="file_name", default="thinvessel.nii.gz",
-                  help="converting/this/file to mesh", metavar = "FILE")
+                  action="store", dest="file_name", default="meshSphere.e",
+                  help="getting nodes from this file", metavar = "FILE")
 parser.add_option( "--output",
                   action="store", dest="output", default="mytetmesh",
                   help="converting/this/file to mesh", metavar = "FILE")
 (options, args) = parser.parse_args()
 if (options.file_name):
-  c3dcmd = "c3d -verbose %s -o %simage.vtk  -sdt -as A -cmv -push A  -omc %ssetup.nii.gz" % (options.file_name,options.output,options.output)
-  print c3dcmd 
-  os.system( c3dcmd )
   
-  imagedata = nib.load("%ssetup.nii.gz" % options.output)
-  zooms = imagedata.header.get_zooms()
-  numpyimage= imagedata.get_data()
-  
+  ## load nodes
+  vtkExodusIIReader = vtk.vtkExodusIIReader()
+  vtkExodusIIReader.SetFileName( options.file_name )
+  vtkExodusIIReader.Update()	  
+  exomesh =  vtkExodusIIReader.GetOutput() 
+  exomesh.IsA("vtkMultiBlockDataSet")
+  myiter = exomesh.NewIterator()
+  myiter.UnRegister(None)
+  myiter.InitTraversal()
+  curInput = myiter.GetCurrentDataObject()
+  mynodes = curInput.GetPoints()
+    
+  # loop over blocks...
+  #while not myiter.IsDoneWithTraversal():
+  #    curInput = myiter.GetCurrentDataObject()
+
+  #exomesh.GetNumberOfElements()
+
+  # FIXME notice that order of operations is IMPORTANT
+  # FIXME translation followed by rotation will give different results
+  # FIXME than rotation followed by translation
+  # FIXME Translate -> RotateZ -> RotateY -> RotateX -> Scale seems to be the order of paraview
+  AffineTransform = vtk.vtkTransform()
+  # should be in meters
+  convertmm = 0.001
+  AffineTransform.Translate(0,0,0 )
+  AffineTransform.RotateZ( 0 )
+  AffineTransform.RotateY( 0 )
+  AffineTransform.RotateX( 0 )
+  AffineTransform.Scale([convertmm ,convertmm ,convertmm ])
+
   ## load vessel
   vesselreader = vtk.vtkDataSetReader();
-  vesselreader.SetFileName("testline.vtk");
+  vesselreader.SetFileName("Centerlinemodel.vtk");
   vesselreader.Update();
-  VesselData = vesselreader.GetOutput();
+  vtkTransformFEMMesh = vtk.vtkTransformFilter()
+  vtkTransformFEMMesh.SetTransform( AffineTransform )
+  vtkTransformFEMMesh.SetInputData( vesselreader.GetOutput() )
+  vtkTransformFEMMesh.Update()
+  vtkClean = vtk.vtkCleanPolyData()
+  vtkClean.SetTolerance(0.05)
+  vtkClean.SetInputData( vtkTransformFEMMesh.GetOutput() )
+  vtkClean.Update( )
+  VesselData = vtkClean.GetOutput();
 
-  ## binaryReader = vtk.vtkDataSetReader()
-  ## binaryReader.SetFileName( "%simage.vtk" % options.output )
-  ## binaryReader.Update()
+  vesselwriter = vtk.vtkDataSetWriter();
+  vesselwriter.SetFileName("Centerlinemodeltransform.vtk" )
+  vesselwriter.SetInputData(VesselData )
+  vesselwriter.Update()
 
-  ## #surfExtractor = vtk.vtkMarchingCubes()
-  ## surfExtractor = vtk.vtkContourFilter()
-  ## surfExtractor.SetInputData(binaryReader.GetOutput())
-  ## surfExtractor.SetValue(0,.7)
-  ## surfExtractor.Update()
-  ## surface = surfExtractor.GetOutput() 
-
-  ## stlwriter = vtk.vtkSTLWriter()
-  ## stlwriter.SetInputData( surface )
-  ## stlwriter.SetFileName( "%simage.stl" % options.output )
-  ## stlwriter.SetFileTypeToASCII()
-  ## #stlwriter.SetFileTypeToBinary()
-  ## stlwriter.Write()
-
-
-  ## for iii in  range(numnodes):
-  ##    nodes.append( (float(lines[iii+1][1]),float(lines[iii+1][2]),float(lines[iii+1][3]), .1) )
-  
-  #image data
-  dim = numpyimage.shape
-
-  mmconversion = 1.e-3
-  pixelsize = (mmconversion* zooms[0] ,mmconversion* zooms[1] ,mmconversion* zooms[2] )
-  
   nodes=[]
-  for iii in range(dim[0]):
-    for jjj in range(dim[1]):
-       for kkk in range(dim[2]):
-           newnode = (pixelsize[0] * numpyimage[iii,jjj,kkk,0,0],
-                      pixelsize[1] * numpyimage[iii,jjj,kkk,0,1],
-                      pixelsize[2] * numpyimage[iii,jjj,kkk,0,2],
-                                     numpyimage[iii,jjj,kkk,0,3])
-           nodes.append(newnode)
+  for iii in range(mynodes.GetNumberOfPoints() ):
+    nodes.append(mynodes.GetPoint(iii))
   
   coarsenode=[]
-  for iii in range(0,dim[0],32):
-    for jjj in range(0,dim[1],32):
-       for kkk in range(0,dim[2],2):
-           newnode = (pixelsize[0] * numpyimage[iii,jjj,kkk,0,0],
-                      pixelsize[1] * numpyimage[iii,jjj,kkk,0,1],
-                      pixelsize[2] * numpyimage[iii,jjj,kkk,0,2])
-           coarsenode.append(newnode)
+  for iii in range(mynodes.GetNumberOfPoints() ):
+    coarsenode.append(mynodes.GetPoint(iii))
 
   # add surfacepoints
   for ipoint in range( VesselData.GetPoints().GetNumberOfPoints() ):
       CurrentPoint = VesselData.GetPoint(ipoint)
-      newnode = (CurrentPoint[0] ,
-                 CurrentPoint[1] ,
-                 CurrentPoint[2] )
+      newnode = ( CurrentPoint[0] ,
+                  CurrentPoint[1] ,
+                  CurrentPoint[2] )
       coarsenode.append(newnode)
 
-  
-  distance = numpyimage[:,:,:,:,3]
-  distancemax = np.max(distance[:])
-  distancemin = max(np.min(distance[:]),0)
-  
+  ## 
+  ## distance = numpyimage[:,:,:,:,3]
+  ## distancemax = np.max(distance[:])
+  ## distancemin = max(np.min(distance[:]),0)
+  ## 
   f = open("%sbg.node" % options.output, "w")
   f.write("%d 3 0 0\n" % len(nodes))
   for iii, node in enumerate(nodes):
@@ -111,44 +106,47 @@ if (options.file_name):
   f = open("%s.1.b.mtr" % options.output, "w")
   f.write("%d 1 \n" % len(nodes))
   for iii, node in enumerate(nodes):
-    #f.write("%12.5e\n" % ( 0.5*pixelsize[0]*node[4] ) )
-    f.write("%12.5e\n" % ( max(pixelsize[0],(1. - np.exp(-5*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    #f.write("%12.5e\n" % ( max(pixelsize[0],(1. - np.exp(-5*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    f.write("%12.5e\n" % 1. )
   f.close()
   
   f = open("%s.2.b.mtr" % options.output, "w")
   f.write("%d 1 \n" % len(nodes))
   for iii, node in enumerate(nodes):
-    f.write("%12.5e\n" % ( max(0.5*pixelsize[0],(1. - np.exp(-4*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    #f.write("%12.5e\n" % ( max(0.5*pixelsize[0],(1. - np.exp(-4*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    f.write("%12.5e\n" % 1. )
   f.close()
   
   f = open("%s.3.b.mtr" % options.output, "w")
   f.write("%d 1 \n" % len(nodes))
   for iii, node in enumerate(nodes):
-    f.write("%12.5e\n" % ( max(0.5*pixelsize[0],(1. - np.exp(-3*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    #f.write("%12.5e\n" % ( max(0.5*pixelsize[0],(1. - np.exp(-3*node[3]/distancemax)) * 32.0 * pixelsize[0] ) ) )
+    f.write("%12.5e\n" % 1. )
   f.close()
   
   
   # run adaptive mesh generation
   backgroundmeshcmd = "tetgen -k %sbg.node;ln -sf %sbg.1.node  %s.1.b.node ; ln -sf %sbg.1.ele   %s.1.b.ele ;ln -sf %sbg.1.node  %s.2.b.node ; ln -sf %sbg.1.ele   %s.2.b.ele ;ln -sf %sbg.1.node  %s.3.b.node ; ln -sf %sbg.1.ele   %s.3.b.ele " % (options.output, options.output,options.output, options.output,options.output, options.output,options.output, options.output,options.output, options.output,options.output, options.output,options.output)
   initializecoarsemeshcmd = "tetgen -k %s.node"       % options.output
-  adaptivemeshcmd = "tetgen -rmqk %s.1.node;tetgen -rmqk %s.2.node; tetgen -rmqk %s.3.node" % (options.output, options.output, options.output)
+  #adaptivemeshcmd = "tetgen -rmqk %s.1.node;tetgen -rmqk %s.2.node; tetgen -rmqk %s.3.node" % (options.output, options.output, options.output)
+  adaptivemeshcmd = "tetgen -rmqk %s.1.node" % options.output
                     
-  print backgroundmeshcmd 
-  print initializecoarsemeshcmd 
-  print adaptivemeshcmd 
+  print(backgroundmeshcmd )
+  print(initializecoarsemeshcmd )
+  print(adaptivemeshcmd )
   os.system(backgroundmeshcmd )
   os.system(initializecoarsemeshcmd )
-  os.system(adaptivemeshcmd )
+  #os.system(adaptivemeshcmd )
 
   
   # load vtk data
   #for iii in [1,2,3,4]:
-  for iii in [2,3]:
+  for iii in [1]:
      vtkReader = vtk.vtkUnstructuredGridReader()
      vtkReader.SetFileName( "%s.%d.vtk" % (options.output,iii) )
      vtkReader.Update()
      vtkMesh  = vtkReader.GetOutput()
-     print 'setup metadata' ,iii
+     print('setup metadata' ,iii)
 
      ## vtkNew<vtkDummyController> controller;
      ## controller->Initialize(&argc, &argv, 1);
@@ -158,13 +156,13 @@ if (options.file_name):
      controller =vtk.vtkDummyController()
      vtk.vtkMultiProcessController.SetGlobalController(controller)
 
-     print 'convert to exodus' ,iii
+     print('convert to exodus' ,iii)
      # convert to exodus 
      myexowriter = vtk.vtkExodusIIWriter()
      myexowriter.DebugOn()
      myexowriter.SetFileName("%s.%d.exo" % (options.output,iii))
      myexowriter.SetInputData( vtkMesh )
-     print 'writing 1' ,"%s.%d.exo" % (options.output,iii)
+     print('writing 1' ,"%s.%d.exo" % (options.output,iii))
      myexowriter.Update()
      ## FIXME - need to setup node sets
      ## ## # create a 3-int array
@@ -191,7 +189,7 @@ if (options.file_name):
 
 else:
   parser.print_help()
-  print options
+  print(options)
 ## with open("isosurface.1.node") as myfile:
 ##     lines = [list(filter(len,line.strip().split(' '))) for line in myfile]
 ## numnodes = int(lines[0][0] )
